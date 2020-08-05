0e3a76885cccf9d0594e4e956adc603776297823
atomr <wildkayote@yandex.ru>
2020-08-05 22:18:47 +0500

    Updates

---
+++ precache-manifest.20523dc22d009274ec0d85799c5ba9b6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "552ed0cfbeac633f06586be84acd59da",
+    "url": "/check-weather/index.html"
+  },
+  {
+    "revision": "330d92f7c547cd4eef99",
+    "url": "/check-weather/static/css/main.d939df25.chunk.css"
+  },
+  {
+    "revision": "b0c4b34c07ef39d6d8e5",
+    "url": "/check-weather/static/js/2.19b1628a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/check-weather/static/js/2.19b1628a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "330d92f7c547cd4eef99",
+    "url": "/check-weather/static/js/main.7730cdc8.chunk.js"
+  },
+  {
+    "revision": "5ef43b169411af0dbca2",
+    "url": "/check-weather/static/js/runtime-main.abbfcd76.js"
+  },
+  {
+    "revision": "d8b3f08b8a6fafbd91b54fd1515c0afd",
+    "url": "/check-weather/static/media/loading.d8b3f08b.svg"
+  }
+]);
